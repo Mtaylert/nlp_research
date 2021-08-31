693cce9a2862acdb7978726238ee33c23bf4abe3
Mtaylert <matthew.taylert@datarobot.com>
2021-08-30 20:23:16 -0400

    successful run of BI-LSTM

--- NER/NER_Modeling.py
+++ NER/NER_Modeling.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
-import torch
-from keras import Model, Input
-from keras.layers import LSTM, Dense, Embedding, Flatten, SpatialDropout1D, TimeDistributed, Bidirectional
-from keras.models import Sequential
-from keras.preprocessing.sequence import pad_sequences
-from keras.preprocessing.text import Tokenizer
-from keras.utils.np_utils import to_categorical
+from tensorflow.keras import Model, Input
+from tensorflow.keras.layers import LSTM, Embedding, Dense
+from tensorflow.keras.layers import TimeDistributed, SpatialDropout1D, Bidirectional
+from tensorflow.keras import Model, Input
+from tensorflow.keras.layers import LSTM, Embedding, Dense
+from tensorflow.keras.layers import TimeDistributed, SpatialDropout1D, Bidirectional
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 
 
 class SentenceGetter(object):
@@ -38,21 +40,30 @@ class NER_Setup():
         words = list(set(self.data['Word']))
         words.append("ENPAD")
         self.num_words = len(words)
+        print(self.num_words)
         tags = list(set(self.data['Tag'].values))
         self.num_tags = len(tags)
         word2idx, tag2idx = id_word_tag(words, tags)
         getter = SentenceGetter(self.data)
         self.sentences = getter.sentences
 
-        self.X = [[word2idx[w[0]] for w in s] for s in self.sentences]
-        self.X = pad_sequences(maxlen=self.max_len, sequences=self.X, padding='post', value=self.num_words + 1)
+        X = [[word2idx[w[0]] for w in s] for s in self.sentences]
+        X = pad_sequences(maxlen=self.max_len, sequences=X, padding='post', value=self.num_words - 1)
 
-        self.y = [[tag2idx[w[2]] for w in s] for s in self.sentences]
-        self.y = pad_sequences(maxlen=self.max_len, sequences=self.y, padding='post', value=tag2idx['O'])
-        self.y = [to_categorical(i, num_classes=self.num_tags) for i in self.y]
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, random_state=123, test_size=.2)
+        y = [[tag2idx[w[2]] for w in s] for s in self.sentences]
+        y = pad_sequences(maxlen=self.max_len, sequences=y, padding='post', value=tag2idx['O'])
+        y = [to_categorical(i, num_classes=self.num_tags) for i in y]
 
-    def build_model(self):
+
+
+        X_train, X_test, y_train, y_test = train_test_split(X, np.array(y), random_state=123, test_size=.2)
+        return (X_train, X_test, y_train, y_test)
+
+    def train(self,X,y):
+
+
+
+
 
         input_word = Input(shape=(self.max_len,))
         model = Embedding(input_dim=self.num_words, output_dim=self.max_len, input_length=self.max_len)(input_word)
@@ -63,6 +74,17 @@ class NER_Setup():
         print(model.summary())
         model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
+        early_stopping = EarlyStopping(monitor='val_accuracy', patience=1, verbose=0, mode='max',
+                                       restore_best_weights=False)
+        callbacks = [early_stopping]
+
+        history = model.fit(X, y, epochs=3, batch_size=32, verbose=1, validation_split=0.2)
+
+        return history
+
+
+
+
 
 
 
@@ -71,5 +93,5 @@ if __name__ == '__main__':
     data = pd.read_csv('ner_dataset.csv', encoding='latin1')
     MAX_LEN = 50
     NER = NER_Setup(data = data, max_len=MAX_LEN)
-    NER.preprocess()
-    NER.build_model()
+    X_train,X_test,y_train, y_test = NER.preprocess()
+    clf_tensor = NER.train(X_train,y_train)
