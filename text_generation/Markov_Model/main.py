4e762561d150783d67be094aa35aa4360ce0c667
Mtaylert <matthew.taylert@datarobot.com>
2021-09-09 15:23:26 -0400

    complete markov project

--- text_generation/Markov_Model/main.py
+++ text_generation/Markov_Model/main.py
@@ -1,19 +1,18 @@
-import pandas as pd
-from text_preprocess import *
+from text_preprocess import TextProcess
 from Markov_Chain import MarkovChain
-import numpy as np
-data = pd.read_csv("../data/shortjokes.csv")
-input_text = '. '.join(data['Joke'])
-
-jokes_text = TextProcess(input_text)
 
 
-mchain_model = MarkovChain(jokes_text, n = 3)
 
-prefixes = ['the people are', 'remember to bring']
-temperatures = [1, 0.7, 0.4, 0.1]
+if __name__ == '__main__':
+    with open("../data/education_section_KAGGLE.txt") as f:
+        data = f.read().split('\n')
 
-for temp in temperatures:
-    print('temperature', temp)
-    print(mchain_model.generate_sequence(np.random.choice(prefixes), 15 ,temperature=temp))
-    print('\n')
+    input_text = '. '.join(data)
+    education_text = TextProcess(input_text)
+    mchain_model = MarkovChain(education_text, n=3)
+    prefixes = ['B.Sc in Maths','Bachelor of Engineering','Master of Science']
+
+    for r in range(10):
+        for x in prefixes:
+            text = mchain_model.generate_sequence(x, 10, temperature=1)
+            print(text)
