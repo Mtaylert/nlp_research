545dfefb282b97042691b5e38420053f898cc71f
Mtaylert <matthew.taylert@datarobot.com>
2021-09-10 09:05:20 -0400

    adding spacy classification example

---
+++ spacy_classifier/README.md
@@ -0,0 +1,7 @@
+## Spacy Classifier Setup
+
+[Configuration Link](https://spacy.io/usage/training#quickstart)
+
+`touch base_config.cfg`
+- Paste configuration into `base_config.cfg`
+- Open terminal and change to working directory `python -m spacy init fill-config ./base_config.cfg ./config.cfg`
---
+++ spacy_classifier/reviews.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import spacy
+from spacy.tokens import DocBin
+from tqdm import tqdm
+
+
+
+class SpacySetup:
+    def __init__(self, data: pd.DataFrame, spacy_model:str=None):
+        if spacy_model:
+            self.nlp = spacy.load(spacy_model)
+        else:
+            self.nlp = spacy.load("en_core_web_sm")
+
+        self.data = data
+        self.zipped_data = self.zip_dataset()
+        self.spacy_docs = self.make_spacy_docs()
+
+
+    def __repr__(self):
+        return self.data
+
+    def zip_dataset(self):
+        zipped_data = tuple(zip(self.data['Review Text'].tolist(), self.data['Recommended IND'].tolist()))
+        return zipped_data
+    def make_spacy_docs(self):
+        docs = []
+
+        for doc, label in tqdm(self.nlp.pipe(self.zipped_data, as_tuples=True)):
+            if label == 1:
+                doc.cats['positive'] = 1
+                doc.cats['negative'] = 0
+
+            else:
+                doc.cats['positive'] = 0
+                doc.cats['negative'] = 1
+
+            docs.append(doc)
+        return docs
+
+
+
+reviews=pd.read_csv("https://raw.githubusercontent.com/hanzhang0420/Women-Clothing-E-commerce/master/Womens%20Clothing%20E-Commerce%20Reviews.csv")
+reviews = reviews[['Review Text','Recommended IND']].dropna()
+model = SpacySetup(reviews)
+
+doc_bin = DocBin(docs=model.spacy_docs)
+doc_bin.to_disk("data/train.spacy")
+
+doc_bin = DocBin(docs=model.spacy_docs)
+doc_bin.to_disk("data/valid.spacy")
+
+
+
