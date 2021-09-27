68c1e8583f487769ea19be55ae27b8bcf49f7a17
Mtaylert <matthew.taylert@datarobot.com>
2021-09-27 16:54:07 -0400

    adding text line extraction

--- document_extraction/text_extract.py
+++ document_extraction/text_extract.py
@@ -1,15 +1,31 @@
-import pandas as pd
+from typing import Dict, Any, List
 import pdfplumber
-import time
 
 
-start = time.time()
-for doc in [1,2]:
-    with pdfplumber.open(f'data/doc{doc}.pdf') as pdf:
-        page_range = len(pdf.pages)
-        for pg in range(page_range):
-            pg_open = pdf.pages[pg]
-end = time.time()
 
-total = end-start
-print(total)
+
+def extract_text_line(text_line_dict : List[Dict[str,Any]]) -> List[List]:
+    page_text, line_block = [], []
+    prev_top = 0
+    for idx, text_line in enumerate(text_line_dict):
+        if idx !=0:
+            section_break_size = text_line['top'] - prev_top
+            if section_break_size < 1:
+                line_block.append(text_line['text'])
+            else:
+                page_text.append(line_block)
+                line_block = [text_line['text']]
+
+        prev_top = text_line['top']
+    return page_text
+
+
+
+
+with pdfplumber.open(f'data/pdf/astro-ph0001004.pdf') as pdf:
+    for idx, page in enumerate(pdf.pages):
+        line = extract_text_line(page.extract_words())
+        print(line)
+
+
+
