e847b7ba0ef101757cd22c93b0f90f862d23053d
Mtaylert <matthew.taylert@datarobot.com>
2021-10-01 15:21:38 -0400

    adding image embedder

--- multimodal/config.py
+++ multimodal/config.py
@@ -3,3 +3,4 @@
 TEXT_WEIGHTS = 'all-distilroberta-v1'
 IMAGE_WEIGHTS = 'imagenet'
 IMAGE_SIZE = 224
+TRAIN_IMAGE_DATA_PATH = 'data/train_images/'
--- multimodal/generate_embeddings.py
+++ multimodal/generate_embeddings.py
@@ -7,7 +7,8 @@ from typing import List, Dict
 import config
 from tqdm import tqdm
 
-class TextEmbed:
+
+class TextEmbedder:
     def __init__(self, text_data: List[str]):
         self.text_data = text_data
         self.model = SentenceTransformer(config.TEXT_WEIGHTS)
@@ -16,8 +17,7 @@ class TextEmbed:
     def __len__(self):
         return len(self.text)
 
-
-    def _get_embeds_(self) -> Dict[int,np.array]:
+    def _get_embeds_(self) -> Dict[int, np.array]:
 
         text_embed_dict = {}
 
@@ -27,8 +27,43 @@ class TextEmbed:
         return text_embed_dict
 
 
-if __name__ == '__main__':
-    train = pd.read_csv('data/train.csv')
-    text_module = TextEmbed(text_data=train['title'])
-    print(text_module.embedding_dict[0])
+class ImageEmbedder:
+    def __init__(self, image_data: dict):
 
+        ssl._create_default_https_context = ssl._create_unverified_context
+        self.images = image_data
+        self.model = tf.keras.applications.MobileNet(
+            input_shape=(config.IMAGE_SIZE, config.IMAGE_SIZE, 3),
+            include_top=False,
+            weights=config.IMAGE_WEIGHTS,
+        )
+
+        self.image_embedding_dict = self._get_embeds()
+
+    def _get_embeds(self):
+
+        image_embed_dict = {}
+
+        for idx, image_file_name in enumerate(tqdm(self.images)):
+
+            image = tf.keras.preprocessing.image.load_img(
+                f"data/train_images/{image_file_name}",
+                target_size=(config.IMAGE_SIZE, config.IMAGE_SIZE),
+            )
+            input_arr = tf.keras.preprocessing.image.img_to_array(image)
+            input_arr = np.array([input_arr])
+            img_embeddings = self.model(input_arr)
+            meanImgEmb1 = np.mean(img_embeddings, axis=0)
+            meanImgEmb2 = np.mean(meanImgEmb1, axis=0)
+            meanImgEmb = np.mean(meanImgEmb2, axis=0)
+            image_embed_dict[idx] = meanImgEmb
+
+        return image_embed_dict
+
+
+if __name__ == "__main__":
+
+    train = pd.read_csv("data/train.csv")
+    text_module = TextEmbedder(text_data=train["title"])
+    image_module = ImageEmbedder(image_data=train["image"])
+    print(image_module.image_embedding_dict[0])
