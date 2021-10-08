adc45ff3e903220240a664663a491da72f9f827e
Mtaylert <matthew.taylert@datarobot.com>
2021-10-08 16:42:33 -0400

    model challenge

--- multi_class_modeling/Gensim/word2vec_model.py
+++ multi_class_modeling/Gensim/word2vec_model.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn import preprocessing, model_selection, metrics
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
 from typing import List
 
 class EmbedText:
@@ -16,18 +17,25 @@ class EmbedText:
         for idx, text_line in enumerate(self.text):
             embedding_list = [self.model.wv[word] for word in text_line]
             embedding_dict[idx] = np.sum(embedding_list, axis=0)
-        return embedding_dict
+        return pd.DataFrame.from_dict(embedding_dict,orient='index')
 
 if __name__ == '__main__':
     data = pd.read_csv('../data.csv')
-    data.Title = data.Title.str.lower()
+    data.Text = data.Title.str.lower()
     data.Title = data.Title.apply(lambda x: x.split())
     enc_labeler = preprocessing.LabelEncoder()
     Y = enc_labeler.fit_transform(data.Conference)
     text = data.Title
 
     X = EmbedText(text, embedding_modeler=FastText).build_embeding_dict()
-    X_train,X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=.15,random_state=123)
-    clf = LogisticRegression(max_iter=5000)
-    clf.fit(X_train,y_train)
-    print(metrics.accuracy_score(y_test,clf.predict(X_test)))
+    X_train,X_test, y_train, y_test = model_selection.train_test_split(X,Y,test_size=.15,random_state=123)
+
+    scores = {}
+
+    for clf in [('LR',LogisticRegression(max_iter=5000)),  ('GBT', GradientBoostingClassifier(n_estimators=150))]:
+        clf[1].fit(X_train,y_train)
+        accuracy = metrics.accuracy_score(y_test,clf[1].predict(X_test))
+        print(accuracy)
+        scores[clf[0]] = accuracy
+
+    print(scores)
