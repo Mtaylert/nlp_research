7a72cb216b1190928b71f0640a8882c961f65c55
Mtaylert <matthew.taylert@datarobot.com>
2021-12-19 11:29:33 -0500

    run black

--- semantic_similarity/load_data.py
+++ semantic_similarity/load_data.py
@@ -2,27 +2,40 @@ import pandas as pd
 
 
 def load_data():
-    train = 'sts_b/sts-train.csv'
-    test = 'sts_b/sts-test.csv'
-    dev = 'sts_b/sts-dev.csv'
+    """
+    loading and splitting of partitioned dataset for semantic similarity
+
+    """
+
+
+    train = "sts_b/sts-train.csv"
+    test = "sts_b/sts-test.csv"
+    dev = "sts_b/sts-dev.csv"
 
     stored = []
-    for text_set in [('train', train), ('test', test), ('dev', dev)]:
-        with open(text_set[1], 'r') as f:
+    for text_set in [("train", train), ("test", test), ("dev", dev)]:
+        with open(text_set[1], "r") as f:
             data = f.read()
-            for idx, line in enumerate(data.split('\n')):
-                line = tuple(line.split('\t'))
+            for idx, line in enumerate(data.split("\n")):
+                line = tuple(line.split("\t"))
                 try:
-                    stored.append({"index": idx,
-                                   'partition': text_set[0],
-                                   "sentence_1": line[5],
-                                   'sentence_2': line[6],
-                                   'similarity': float(line[4])})
+                    stored.append(
+                        {
+                            "index": idx,
+                            "partition": text_set[0],
+                            "sentence_1": line[5],
+                            "sentence_2": line[6],
+                            "similarity": float(line[4]),
+                        }
+                    )
                 except:
                     pass
     stored = pd.DataFrame(stored)
     return stored
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     df = load_data()
+    v = 10
+    print(df['sentence_1'].iloc[v])
+    print(df['sentence_2'].iloc[v])
