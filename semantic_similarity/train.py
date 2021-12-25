5cdc62fdfb0b234cfbdcd73ef8f81ed18acfe84f
Mtaylert <matthew.taylert@datarobot.com>
2021-12-25 13:47:54 -0500

    adding a fit and predict method

--- semantic_similarity/train.py
+++ semantic_similarity/train.py
@@ -13,86 +13,96 @@ import torch
 from tqdm import tqdm
 
 
-def loss_fn(outputs, targets):
-    return nn.BCEWithLogitsLoss()(outputs, targets.view(-1, 1))
+class SemanticSimilarity:
+    """
+    Passing the transformer architecture into a sklearn arch
+    """
+    def __init__(self, epochs: int = 2, retrain: bool = True):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.epochs = epochs
+        self.save_path = "model/"
+        if retrain:
+            self.model = BertBaseUncased()
+            self.model.to(self.device)
+        else:
+            pass
 
+    def loss_fn(self, outputs, targets):
+        return nn.BCEWithLogitsLoss()(outputs, targets.view(-1, 1))
 
-def train():
-    save_path = "model/"
-    df = load_data()
-    train = df[df["partition"] == "train"]
-    dev = df[df["partition"] == "dev"]
-    test = df[df["partition"] == "test"]
+    def fit(self, X, y):
+        train_dataset = data_setup.BertDatasetTraining(
+            input1=X["sentence_1"],
+            input2=X["sentence_2"],
+            target=y,
+        )
 
-    train_dataset = data_setup.BertDatasetTraining(
-        input1=train["sentence_1"],
-        input2=train["sentence_2"],
-        target=train["similarity"],
-    )
+        train_data_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=config.TRAIN_BATCH_SIZE, num_workers=4
+        )
 
-    dev_dataset = data_setup.BertDatasetTraining(
-        input1=dev["sentence_1"], input2=dev["sentence_2"], target=dev["similarity"]
-    )
+        param_optimizer = list(self.model.named_parameters())
+        no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
+        optimizer_parameters = [
+            {
+                "params": [
+                    p for n, p in param_optimizer if not any(nd in n for nd in no_decay)
+                ],
+                "weight_decay": 0.001,
+            },
+            {
+                "params": [
+                    p for n, p in param_optimizer if any(nd in n for nd in no_decay)
+                ],
+                "weight_decay": 0.0,
+            },
+        ]
 
-    train_data_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=config.TRAIN_BATCH_SIZE, num_workers=4
-    )
-    valid_data_loader = torch.utils.data.DataLoader(
-        dev_dataset, batch_size=config.VALID_BATCH_SIZE, num_workers=1
-    )
+        num_train_steps = int(len(X) / config.TRAIN_BATCH_SIZE * config.EPOCHS)
+        optimizer = AdamW(optimizer_parameters, lr=3e-5)
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer, num_warmup_steps=0, num_training_steps=num_train_steps
+        )
+        self.model.train()
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = BertBaseUncased()
-    model.to(device)
-    param_optimizer = list(model.named_parameters())
-    no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
-    optimizer_parameters = [
-        {
-            "params": [
-                p for n, p in param_optimizer if not any(nd in n for nd in no_decay)
-            ],
-            "weight_decay": 0.001,
-        },
-        {
-            "params": [
-                p for n, p in param_optimizer if any(nd in n for nd in no_decay)
-            ],
-            "weight_decay": 0.0,
-        },
-    ]
+        for epoch in range(self.epochs):
+            for batch_idx, dataset in tqdm(
+                enumerate(train_data_loader), total=len(train_data_loader)
+            ):
+                ids = dataset["ids"]
+                mask = dataset["mask"]
+                token_type_ids = dataset["token_type_ids"]
+                targets = dataset["targets"]
 
-    num_train_steps = int(len(train) / config.TRAIN_BATCH_SIZE * config.EPOCHS)
-    optimizer = AdamW(optimizer_parameters, lr=3e-5)
-    scheduler = get_linear_schedule_with_warmup(
-        optimizer, num_warmup_steps=0, num_training_steps=num_train_steps
-    )
+                ids = ids.to(self.device, dtype=torch.long)
+                mask = mask.to(self.device, dtype=torch.long)
+                token_type_ids = token_type_ids.to(self.device, dtype=torch.long)
+                targets = targets.to(self.device, dtype=torch.float)
 
-    model.train()
-    for epoch in range(config.EPOCHS):
-        for batch_idx, dataset in tqdm(
-            enumerate(train_data_loader), total=len(train_data_loader)
-        ):
-            ids = dataset["ids"]
-            mask = dataset["mask"]
-            token_type_ids = dataset["token_type_ids"]
-            targets = dataset["targets"]
+                optimizer.zero_grad()
+                outputs = self.model(ids, mask=mask, token_type_ids=token_type_ids)
+                loss = self.loss_fn(outputs, targets)
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+            torch.save(
+                self.model.state_dict(),
+                "{}/{}_epoch_MODEL.model".format(self.save_path, epoch),
+            )
 
-            ids = ids.to(device, dtype=torch.long)
-            mask = mask.to(device, dtype=torch.long)
-            token_type_ids = token_type_ids.to(device, dtype=torch.long)
-            targets = targets.to(device, dtype=torch.float)
+        torch.save(
+            self.model.state_dict(), "{}/LAST_MODEL.model".format(self.save_path)
+        )
 
-            optimizer.zero_grad()
-            outputs = model(ids, mask=mask, token_type_ids=token_type_ids)
-
-            loss = loss_fn(outputs, targets)
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
-
-    torch.save(model.state_dict(), "{}/LAST_MODEL.model".format(save_path))
-    return model
+    def predict(self):
+        pass
 
 
 if __name__ == "__main__":
-    model = train()
+    df = load_data()
+    train = df[df["partition"] == "train"]
+    X = train.drop(["similarity"], axis=1)
+    y = train["similarity"]
+
+    clf = SemanticSimilarity()
+    clf.fit(X, y)
