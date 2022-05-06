a764d2efb32b982dfb2f0ef5112c08ad5a71edfe
Matthew Taylert <matthew.taylert@datarobot.com>
2022-05-06 14:41:57 -0400

    rem

--- hugging_face_review/embedding_extraction.py
+++ hugging_face_review/embedding_extraction.py
@@ -1,4 +1,14 @@
-import pandas as pd
-import numpy as np
+from transformers import AutoTokenizer
+from torch import nn
+from transformers import AutoConfig
 
 
+model_cpt = 'bert-base-uncased'
+tokenizer = AutoTokenizer.from_pretrained(model_cpt)
+text = "time flies like an arrow"
+
+config = AutoConfig.from_pretrained(model_cpt)
+inputs = tokenizer(text, return_tensors='pt',add_special_tokens=False)
+token_embed = nn.Embedding(config.vocab_size, config.hidden_size)
+input_embeddings = token_embed(inputs.input_ids)
+print(input_embeddings)
